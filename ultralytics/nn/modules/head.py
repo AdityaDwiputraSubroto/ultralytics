5d9e984df46d71e5211e73817c4bcd1ffa73b749
Aditya Dwiputra Subroto <103212819+AdityaDwiputraSubroto@users.noreply.github.com>
2025-08-05 09:28:13 +0700

    Update head.py
    Signed-off-by: Aditya Dwiputra Subroto <103212819+AdityaDwiputraSubroto@users.noreply.github.com>

--- ultralytics/nn/modules/head.py
+++ ultralytics/nn/modules/head.py
@@ -87,11 +87,11 @@ class Detect(nn.Module):
         self.nc = nc  # number of classes
         self.nl = len(ch)  # number of detection layers
         self.reg_max = 16  # DFL channels (ch[0] // 16 to scale 4/8/12/16/20 for n/s/m/l/x)
-        self.no = nc + self.reg_max * 4  # number of outputs per anchor
+        self.no = nc + 8  # number of outputs per anchor
         self.stride = torch.zeros(self.nl)  # strides computed during build
-        c2, c3 = max((16, ch[0] // 4, self.reg_max * 4)), max(ch[0], min(self.nc, 100))  # channels
+        c2, c3 = max((16, ch[0] // 4, 8)), max(ch[0], min(self.nc, 100))  # channels
         self.cv2 = nn.ModuleList(
-            nn.Sequential(Conv(x, c2, 3), Conv(c2, c2, 3), nn.Conv2d(c2, 4 * self.reg_max, 1)) for x in ch
+            nn.Sequential(Conv(x, c2, 3), Conv(c2, c2, 3), nn.Conv2d(c2, 8, 1)) for x in ch
         )
         self.cv3 = (
             nn.ModuleList(nn.Sequential(Conv(x, c3, 3), Conv(c3, c3, 3), nn.Conv2d(c3, self.nc, 1)) for x in ch)
@@ -165,10 +165,10 @@ class Detect(nn.Module):
             self.shape = shape
 
         if self.export and self.format in {"saved_model", "pb", "tflite", "edgetpu", "tfjs"}:  # avoid TF FlexSplitV ops
-            box = x_cat[:, : self.reg_max * 4]
-            cls = x_cat[:, self.reg_max * 4 :]
+            box = x_cat[:, : 8]
+            cls = x_cat[:, 8 :]
         else:
-            box, cls = x_cat.split((self.reg_max * 4, self.nc), 1)
+            box, cls = x_cat.split((8, self.nc), 1)
 
         if self.export and self.format in {"tflite", "edgetpu"}:
             # Precompute normalization factor to increase numerical stability
@@ -179,7 +179,7 @@ class Detect(nn.Module):
             norm = self.strides / (self.stride[0] * grid_size)
             dbox = self.decode_bboxes(self.dfl(box) * norm, self.anchors.unsqueeze(0) * norm[:, :2])
         else:
-            dbox = self.decode_bboxes(self.dfl(box), self.anchors.unsqueeze(0)) * self.strides
+            dbox = box
         if self.export and self.format == "imx":
             return dbox.transpose(1, 2), cls.sigmoid().permute(0, 2, 1)
         return torch.cat((dbox, cls.sigmoid()), 1)
@@ -513,7 +513,7 @@ class WorldDetect(Detect):
             x[i] = torch.cat((self.cv2[i](x[i]), self.cv4[i](self.cv3[i](x[i]), text)), 1)
         if self.training:
             return x
-        self.no = self.nc + self.reg_max * 4  # self.nc could be changed when inference with different texts
+        self.no = self.nc + 8  # self.nc could be changed when inference with different texts
         y = self._inference(x)
         return y if self.export else (y, x)
 
@@ -734,7 +734,7 @@ class YOLOEDetect(Detect):
         if self.dynamic or self.shape != shape:
             self.anchors, self.strides = (x.transpose(0, 1) for x in make_anchors([b[0] for b in x], self.stride, 0.5))
             self.shape = shape
-        box = torch.cat([xi[0].view(shape[0], self.reg_max * 4, -1) for xi in x], 2)
+        box = torch.cat([xi[0].view(shape[0], 8, -1) for xi in x], 2)
         cls = torch.cat([xi[1] for xi in x], 2)
 
         if self.export and self.format in {"tflite", "edgetpu"}:
@@ -766,7 +766,7 @@ class YOLOEDetect(Detect):
             x[i] = torch.cat((self.cv2[i](x[i]), self.cv4[i](self.cv3[i](x[i]), cls_pe)), 1)
         if self.training:
             return x
-        self.no = self.nc + self.reg_max * 4  # self.nc could be changed when inference with different texts
+        self.no = self.nc + 8  # self.nc could be changed when inference with different texts
         y = self._inference(x)
         return y if self.export else (y, x)
 
